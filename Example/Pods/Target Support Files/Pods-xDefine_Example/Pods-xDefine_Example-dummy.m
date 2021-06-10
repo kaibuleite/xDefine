e128aa1dfe09a7f5ff13dbc9e1c75918ef63ff27
kaibuleite <177955297@qq.com>
2021-06-10 14:23:10 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-xDefine_Example/Pods-xDefine_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xDefine_Example : NSObject
+@end
+@implementation PodsDummy_Pods_xDefine_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-xDefine_Tests/Pods-xDefine_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xDefine_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_xDefine_Tests
+@end
